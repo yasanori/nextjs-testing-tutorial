2b5cf205c30e299d13cb8bad7db466c8992b2c7b
yasanori <itsonlf2525@gmail.com>
2022-01-20 22:16:38 +0900

    add

--- pages/blog-page.tsx
+++ pages/blog-page.tsx
@@ -1,10 +1,24 @@
+import { GetStaticProps } from 'next'
+import { POST } from '../types/Types'
 import Layout from '../components/Layout'
+import { getAllPostsData } from '../lib/fetch'
+import Post from '../components/Post'
 
-const BlogPage: React.FC = () => {
+interface STATICPROPS {
+  posts: POST[]
+}
+
+const BlogPage: React.FC<STATICPROPS> = ({ posts }) => {
   return (
     <Layout title="Blog">
       <p className="text-4xl">blog page</p>
+      <ul>{posts && posts.map((post) => <Post key={post.id} {...post} />)}</ul>
     </Layout>
   )
 }
 export default BlogPage
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getAllPostsData()
+  return { props: { posts } }
+}
