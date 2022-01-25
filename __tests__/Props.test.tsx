75cff4bbea331866a2c41d666e8566761ebe946c
yasanori <itsonlf2525@gmail.com>
2022-01-25 22:41:38 +0900

    add test Props

---
+++ __tests__/Props.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Post from '../components/Post'
+import { POST } from '../types/Types'
+import 'setimmediate'
+
+describe('Post component with given props', () => {
+  let dummyProps: POST
+  beforeEach(() => {
+    dummyProps = {
+      userId: 1,
+      id: 1,
+      title: 'dummy title 1',
+      body: 'dummy body 1',
+    }
+  })
+  it('Should render correctly with given props value', () => {
+    render(<Post {...dummyProps} />)
+    expect(screen.getByText(dummyProps.id)).toBeInTheDocument()
+    expect(screen.getByText(dummyProps.title)).toBeInTheDocument()
+    //screen.debug()
+  })
+})
